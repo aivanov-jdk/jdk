b22d27f917748bc43aae0873436ad0be4ac8b393
Alexey Ivanov <alexey.ivanov@oracle.com>
2022-03-03 15:04:11 +0000

    Don't print stack trace for NPE

--- test/jdk/java/awt/Robot/HiDPIMouseMove/RobotMouseMoveScreens.java
+++ test/jdk/java/awt/Robot/HiDPIMouseMove/RobotMouseMoveScreens.java
@@ -106,9 +106,6 @@ public class RobotMouseMoveScreens {
                             match1++;
                         }
                     } catch (NullPointerException e) {
-                        if (npe == 0) {
-                            e.printStackTrace();
-                        }
                         npe++;
                     }
                 }
