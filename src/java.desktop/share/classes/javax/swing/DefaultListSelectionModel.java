85f32e4c3fb084c2f5ffe437d9c30394e67b4111
Alexey Ivanov <alexey.ivanov@oracle.com>
2022-10-14 21:09:05 +0100

    Optimize imports, update copyright

--- src/java.desktop/share/classes/javax/swing/DefaultListSelectionModel.java
+++ src/java.desktop/share/classes/javax/swing/DefaultListSelectionModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,12 +25,14 @@
 
 package javax.swing;
 
-import java.util.EventListener;
-import java.util.BitSet;
-import java.io.Serializable;
 import java.beans.Transient;
+import java.io.Serializable;
+import java.util.BitSet;
+import java.util.EventListener;
 
-import javax.swing.event.*;
+import javax.swing.event.EventListenerList;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 
 /**
