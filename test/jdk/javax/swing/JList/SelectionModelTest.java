5c6e3d80e3dcec5498c317916b809578510792bb
Alexey Ivanov <alexey.ivanov@oracle.com>
2022-10-26 20:09:16 +0100

    Expect IOOBE for insertIndexInterval(10, -10, false)
    Modified test29 to expect IOOBE in the new implementation.

--- test/jdk/javax/swing/JList/SelectionModelTest.java
+++ test/jdk/javax/swing/JList/SelectionModelTest.java
@@ -620,20 +620,14 @@ public class SelectionModelTest {
     private static void test29() {
         DefaultListSelectionModel selectionModel = createModel();
         selectionModel.setSelectionInterval(11, 11);
-        assertIndexes(selectionModel,
-                      11, 11,
-                      11, 11);
 
-        selectionModel.insertIndexInterval(10, -10, false);
-        assertIndexes(selectionModel,
-                      1, 11,
-                      1, 1);
-        assertTrue(IntStream.rangeClosed(0, 0)
-                            .noneMatch(selectionModel::isSelectedIndex));
-        assertTrue(IntStream.rangeClosed(11, 11)
-                            .allMatch(selectionModel::isSelectedIndex));
-        assertTrue(IntStream.rangeClosed(12, Integer.MAX_VALUE)
-                            .noneMatch(selectionModel::isSelectedIndex));
+        try {
+            selectionModel.insertIndexInterval(10, -10, false);
+            throw new RuntimeException("Expected IndexOutOfBoundsException "
+                                       + "because of negative length");
+        } catch (IndexOutOfBoundsException ignored) {
+            // expected
+        }
     }
 
     private static Exception runTest(Runnable test) {
