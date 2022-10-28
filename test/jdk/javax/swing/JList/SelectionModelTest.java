a18a5eef34059be064eb8b34d3812e43d88f45af
Alexey Ivanov <alexey.ivanov@oracle.com>
2022-10-28 11:37:11 +0100

    Set timeout to 180 sec (3 min)

--- test/jdk/javax/swing/JList/SelectionModelTest.java
+++ test/jdk/javax/swing/JList/SelectionModelTest.java
@@ -35,7 +35,7 @@ import javax.swing.DefaultListSelectionModel;
  *           DefaultListSelectionModel.insertIndexInterval in the edge
  *           cases of integer overflow (Integer.MAX_VALUE)
  * @requires (os.maxMemory >= 5G)
- * @run main/othervm -Xmx4g SelectionModelTest
+ * @run main/othervm/timeout=180000 -Xmx4g SelectionModelTest
  */
 public class SelectionModelTest {
 
