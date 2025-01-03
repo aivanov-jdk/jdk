cd413e19bd6bb8f1c400f6efbe0deaa46a30e74c
Alexey Ivanov <alexey.ivanov@oracle.com>
2025-01-03 17:19:12 +0000

    Move mouse to 4 corners and to center, and read the position

--- test/jdk/java/awt/Robot/MouseMoveOffScreen.java
+++ test/jdk/java/awt/Robot/MouseMoveOffScreen.java
@@ -21,9 +21,15 @@
  * questions.
  */
 
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
 import java.awt.MouseInfo;
 import java.awt.Point;
+import java.awt.PointerInfo;
+import java.awt.Rectangle;
 import java.awt.Robot;
+
+import static java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment;
 
 /*
  * @test
@@ -36,8 +42,44 @@ import java.awt.Robot;
 public class MouseMoveOffScreen {
     private static final Point OFF_SCREEN_LOC = new Point(20000, 200);
 
+    private static void moveMouseAndCheck(final Robot robot,
+                                          final int x, final int y) {
+        System.out.println(x + ", " + y);
+        robot.mouseMove(x, y);
+        robot.delay(1000);
+
+        PointerInfo cursor = MouseInfo.getPointerInfo();
+        if (cursor != null) {
+            System.out.println(cursor.getLocation() + " - "
+                               + cursor.getDevice().getIDstring());
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Robot robot = new Robot();
+
+        GraphicsDevice[] screens = getLocalGraphicsEnvironment()
+                                   .getScreenDevices();
+        for (GraphicsDevice screen : screens) {
+            GraphicsConfiguration gc = screen.getDefaultConfiguration();
+            System.out.println(screen.getIDstring());
+            System.out.println("\t" + gc.getBounds());
+            System.out.println("\t" + gc.getDefaultTransform());
+        }
+        System.out.println("\n");
+
+        for (GraphicsDevice screen : screens) {
+            System.out.println(screen.getIDstring());
+            Rectangle bounds = screen.getDefaultConfiguration()
+                                     .getBounds();
+
+            moveMouseAndCheck(robot, bounds.x, bounds.y);
+            moveMouseAndCheck(robot, bounds.x + bounds.width - 1, bounds.y);
+            moveMouseAndCheck(robot, bounds.x, bounds.y + bounds.height - 1);
+            moveMouseAndCheck(robot, bounds.x + bounds.width - 1, bounds.y + bounds.height - 1);
+            moveMouseAndCheck(robot, bounds.x + bounds.width / 2, bounds.y + bounds.height / 2);
+        }
+
         robot.mouseMove(200, 200);
         robot.delay(500);
         robot.mouseMove(OFF_SCREEN_LOC.x, OFF_SCREEN_LOC.y);
