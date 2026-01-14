edbffdb0b947c056afb63c23ca6539d24c09e586
Alexey Ivanov <alexey.ivanov@oracle.com>
2026-01-14 16:20:26 +0000

    Bump the copyright to 2026
    Also amend the summary to better capture what the test does.

--- test/jdk/java/awt/print/PrinterJob/PageRangesAuto.java
+++ test/jdk/java/awt/print/PrinterJob/PageRangesAuto.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -44,7 +44,7 @@ import javax.print.attribute.standard.PageRanges;
  * @test
  * @bug 6575331 8297191
  * @key printer
- * @summary Verifies all the pages in a range are printed
+ * @summary Automatically verifies all the pages in a range are printed
  * @run main PageRangesAuto
  */
 public class PageRangesAuto implements Pageable, Printable {
