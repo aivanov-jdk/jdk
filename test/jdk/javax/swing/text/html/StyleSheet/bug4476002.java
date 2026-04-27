a320cd322f28090c50b9b3515217e6da663e791c
Alexey Ivanov <alexey.ivanov@oracle.com>
2026-04-27 17:04:18 +0100

    8239489: Refactor bug4476002 to verify the expected colors only
    The original test verifies there are no black pixels.
    If Robot fails captures the wrong area of the screen, the test will
    not fail. (And it happens in 8u in High DPI environments.)
    
    The updated test checks for the expected colors:
    the background color of the component and red text.
    To make sure there are no other colors in the rendered text,
    it turns off text antialiasing off.

--- test/jdk/javax/swing/text/html/StyleSheet/bug4476002.java
+++ test/jdk/javax/swing/text/html/StyleSheet/bug4476002.java
@@ -24,76 +24,90 @@
 /*
  * @test
  * @bug 4476002
- * @summary  Verifies JEditorPane: <ol> list numbers do not pick up color of the list text
- * @key headful
- * @run main bug4476002
+   @summary Tests the color of the bullet in HTML-formatted label
+            is the same as the color of text
+   @key headful
+   @run main/othervm -Dawt.useSystemAAFontSettings=off -Dsun.java2d.dpiaware=true bug4476002
 */
 
+import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Robot;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.concurrent.CountDownLatch;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
-public class bug4476002 {
-
-    private static boolean passed = true;
-    private static JLabel htmlComponent;
-
-    private static Robot robot;
-    private static JFrame mainFrame;
-    private static volatile Point p;
-    private static volatile Dimension d;
-
-    public static void main(String[] args) throws Exception {
-        robot = new Robot();
-
-        try {
-            SwingUtilities.invokeAndWait(() -> {
-                String htmlText =
-                    "<html><head><style>" +
+public class bug4476002 extends WindowAdapter {
+    private static final String HTML_LABEL =
+            "<html><head><style>" +
                     "OL { list-style-type: disc; color: red }" +
                     "</style></head>" +
-                    "<body><ol><li>wwwww</li></ol></body></html>";
+                    "<body><ol><li>wwww</li></ol></body></html>";
 
-                mainFrame = new JFrame("bug4476002");
+    private JFrame mainFrame;
+    private JLabel htmlComponent;
 
-                htmlComponent = new JLabel(htmlText);
-                mainFrame.getContentPane().add(htmlComponent);
+    private final Robot robot;
 
-                mainFrame.pack();
-                mainFrame.setLocationRelativeTo(null);
-                mainFrame.setVisible(true);
-            });
-            robot.waitForIdle();
-            robot.delay(1000);
-            SwingUtilities.invokeAndWait(() -> {
-                p = htmlComponent.getLocationOnScreen();
-                d = htmlComponent.getSize();
-            });
-            int x0 = p.x;
-            int y = p.y + d.height/2;
+    private static final CountDownLatch latch = new CountDownLatch(1);
 
-            for (int x = x0; x < x0 + d.width; x++) {
-                if (robot.getPixelColor(x, y).equals(Color.black)) {
-                    passed = false;
-                    break;
-                }
-            }
-            if (!passed) {
-                throw new RuntimeException("Test failed.");
-            }
+    public static void main(String[] args) throws Exception {
+        final bug4476002 testCase = new bug4476002();
+        try {
+            SwingUtilities.invokeLater(testCase::createUI);
+            latch.await();
+            testCase.robot.waitForIdle();
+            Thread.sleep(500);
+            SwingUtilities.invokeAndWait(testCase::runTest);
         } finally {
-            SwingUtilities.invokeAndWait(() -> {
-                if (mainFrame != null) {
-                    mainFrame.dispose();
-                }
-            });
+            SwingUtilities.invokeLater(testCase::closeFrame);
         }
     }
 
+    private bug4476002() throws AWTException {
+        robot = new Robot();
+    }
+
+    private void createUI() {
+        mainFrame = new JFrame("bug4476002");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.addWindowListener(this);
+
+        htmlComponent = new JLabel(HTML_LABEL);
+        mainFrame.getContentPane().add(htmlComponent);
+
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+
+    private void closeFrame() {
+        mainFrame.dispose();
+    }
+
+    private void runTest() {
+        Point p = htmlComponent.getLocationOnScreen();
+        Dimension d = htmlComponent.getSize();
+
+        final Color background = htmlComponent.getBackground();
+
+        int x0 = p.x;
+        int y = p.y + d.height / 2;
+
+        for (int x = x0; x < x0 + d.width; x++) {
+            Color pixel = robot.getPixelColor(x, y);
+            if (!(pixel.equals(Color.red) || pixel.equals(background))) {
+                throw new RuntimeException("Unexpected color: " + pixel);
+            }
+        }
+    }
+
+    @Override
+    public void windowOpened(WindowEvent ev) {
+        latch.countDown();
+    }
 }
